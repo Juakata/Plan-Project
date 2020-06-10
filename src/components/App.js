453c3a6ea23b08c132dc636c2fb595df492dac9c
Juakata <juakata07@gmail.com>
2020-06-10 14:11:01 -0600

    add links with Switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from './layout/Navbar.js';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './home';
+import SignIn from './auth/SignIn';
+import SignOut from './auth/SignOut';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
-    <Route to="/" component={Home} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signout" component={SignOut} />
+    </Switch>
   </BrowserRouter>
 );
 
---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+  return (
+    <form>
+      <input type="text" />
+    </form>
+  );
+}
+
+export default SignIn;
