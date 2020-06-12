de36fdb1dcab2cde0baafc10137c6557cccf7ede
Juakata <juakata07@gmail.com>
2020-06-12 10:53:17 -0600

    add signin form

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,11 +1,58 @@
 import React from 'react';
 
-const SignIn = () => {
-  return (
-    <form>
-      <input type="text" />
-    </form>
-  );
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = event => {
+    const { id, value } = event.target;
+    this.setState({ [id]: value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className="row">
+        <form className="col s12" onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                id="email"
+                type="email"
+                className="validate"
+                onChange={this.handleChange}
+                value={email}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                id="password"
+                type="password"
+                className="validate"
+                onChange={this.handleChange}
+                value={password}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <button type="submit" className="waves-effect waves-light btn">Create</button>
+        </form>
+      </div>
+    );
+  };
 }
 
 export default SignIn;
