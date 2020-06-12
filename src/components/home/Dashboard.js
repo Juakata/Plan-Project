641e4a50fca4b4135eebeaee87184ab657e594ee
Juakata <juakata07@gmail.com>
2020-06-12 15:37:25 -0600

    connect firebase to redux

--- src/components/home/Dashboard.js
+++ src/components/home/Dashboard.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import ProjectList from '../projects/ProjectList';
 
 const Dashboard = ({ projects }) => (
   <div>
+    {console.log(projects)}
     <ProjectList projects={projects}/>
   </div>
 );
 
 const mapStateToProps = state => ({
-  projects: state.project.projects,
+  projects: state.firestore.ordered.projects,
 });
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects' },
+  ]),
+)(Dashboard);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './authReducer';
 import project from './projectReducer';
+import { firestoreReducer } from 'redux-firestore';
 
 export default combineReducers({
     auth,
     project,
+    firestore: firestoreReducer,
 });
