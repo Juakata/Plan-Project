b1aa834cba82b69c955c5ed6c05f6053aa34e45f
Juakata <juakata07@gmail.com>
2020-06-15 13:29:39 -0600

    load correct links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,20 +4,19 @@ import { NavLink } from 'react-router-dom'
 import SignInLinks from './SignInLinks';
 import SignOutLinks from './SignOutLinks';
 
-const Navbar = () => (
+const Navbar = ({ auth }) => (
   <nav className="nav-top nav-wrapper grey darken-3">
     <div className="container">
       <NavLink to="/" className="brand-logo">Plan Project</NavLink>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <SignInLinks />
-        <SignOutLinks />
+        {auth.isLoaded ? auth.uid ? <SignInLinks /> : <SignOutLinks /> : null}
       </ul>
     </div>
   </nav>
 );
 
 const mapStateToProps = state => ({
-  
+  auth: state.firebase.auth,
 });
 
 export default connect(mapStateToProps)(Navbar);
