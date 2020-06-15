8ab64c6cc5ba533310f92ca99320170b1c40528f
Juakata <juakata07@gmail.com>
2020-06-15 11:23:34 -0600

    add authReducers

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,10 @@
-export default (state = {}, action) => {
-  return state;
+export default (state = { authError: null }, action) => {
+  switch (action.type) {
+    case 'LOGIN_ERROR':
+      return { ...state, authError: 'Login failed' };
+    case 'LOGIN_SUCCESS':
+      return { ...state, authError: null };
+    default:
+      return state;
+  }
 }
