af251f2f705d80cfc1a5fff66e52bacdeebdac7b
Juakata <juakata07@gmail.com>
2020-06-15 12:41:48 -0600

    add signout reducers

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,8 +1,12 @@
 export default (state = { authError: null }, action) => {
   switch (action.type) {
     case 'LOGIN_ERROR':
-      return { ...state, authError: 'Login failed' };
+      return { ...state, authError: 'Login Failed' };
     case 'LOGIN_SUCCESS':
+      return { ...state, authError: null };
+    case 'SIGNOUT_ERROR':
+      return { ...state, authError: 'Logout Failed' };
+    case 'SIGNOUT_SUCCESS':
       return { ...state, authError: null };
     default:
       return state;
