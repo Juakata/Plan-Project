60096d58ac675f3bb0eb77c6cd841ebcf6184006
Juakata <juakata07@gmail.com>
2020-06-16 12:53:14 -0600

    add functions using firebase functions

--- src/components/home/Notifications.js
+++ src/components/home/Notifications.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Notifications = () => (
-  <div>Notifications</div>
+  <div className="section">
+    <div className="card z-depth-0">
+      <div className="card-content">
+        <span className="card-title">Notifications</span>
+        <ul className="notifications">
+          <li>Notification 1</li>
+          <li>Notification 2</li>
+          <li>Notification 3</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 );
 
 export default Notifications;
